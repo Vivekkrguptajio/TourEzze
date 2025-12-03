beebe37bef329be1ecc6c1cf3e025a254be3d53e
itamitbhai <yadavamit57354@gmail.com>
2025-12-03 15:26:34 +0530

    UI Updated

--- Frontend/src/RolePages/Tourist/components/Plan/Plan.jsx
+++ Frontend/src/RolePages/Tourist/components/Plan/Plan.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AiItineraryPro from './components/AiItineraryPro'
 
+//Plan function added
 export default function Plan() {
   return (
     <div><AiItineraryPro/></div>
